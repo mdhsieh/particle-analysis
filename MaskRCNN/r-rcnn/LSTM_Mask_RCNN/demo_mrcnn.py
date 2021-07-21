082d7631ff30f642be400a3992bc677bb9265c3f
Michael Hsieh <mdhsieh8@gmail.com>
2021-07-20 19:10:27 -0700

    Replace LSTM_Mask_RCNN folder and inference script with latest ones.
    lstm_mask_rcnn_train.ipynb runs demo_mrcnn.py and demo_rrcnn.py
    demo_mrcnn.py cannot detect images in demo_video_nucleus/frames folder.
    demo_rrcnn.py does not output any results.

--- MaskRCNN/r-rcnn/LSTM_Mask_RCNN/demo_mrcnn.py
+++ MaskRCNN/r-rcnn/LSTM_Mask_RCNN/demo_mrcnn.py
@@ -95,3 +95,115 @@ print("# Processed Frames: %d" % frame_number)
 print("# Cost Time: %.3f" % total_mrcnn_time)
 print("# FPS: %.1f" % fps)
 print("# ------------------------------------ #\n")
+
+# new script using nucleus.py from https://github.com/matterport/Mask_RCNN/tree/master/samples/nucleus
+'''
+import os
+import sys
+import random
+import math
+import re
+import time
+import numpy as np
+import tensorflow as tf
+import matplotlib
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+
+# go to samples/nucleus folder in LSTM_Mask_RCNN repo
+sys.path.append("samples/nucleus")
+# import nucleus is only available in samples/nucleus directory
+import nucleus
+
+# Root directory of the project
+# ROOT_DIR = os.path.abspath("../../")
+ROOT_DIR = os.getcwd()
+print("root dir:", ROOT_DIR)
+
+# Import Mask RCNN
+sys.path.append(ROOT_DIR)  # To find local version of the library
+from mrcnn import utils
+from mrcnn import visualize
+from mrcnn.visualize import display_images
+import mrcnn.model as modellib
+from mrcnn.model import log
+
+# %matplotlib inline 
+
+# Directory to save logs and trained model
+LOGS_DIR = os.path.join(ROOT_DIR, "logs")
+
+# Dataset directory
+# DATASET_DIR = os.path.join(ROOT_DIR, "single-particle-dataset/nucleus")
+DATASET_DIR = os.path.join(ROOT_DIR, "demo_video_nucleus/frames")
+
+# Inference Configuration
+config = nucleus.NucleusInferenceConfig()
+config.display()
+
+# Device to load the neural network on.
+DEVICE = "/cpu:0"  # /cpu:0 or /gpu:0
+
+# Inspect the model in training or inference modes
+# values: 'inference' or 'training'
+# Only inference mode is supported right now
+TEST_MODE = "inference"
+
+def get_ax(rows=1, cols=1, size=16):
+    """Return a Matplotlib Axes array to be used in
+    all visualizations in the notebook. Provide a
+    central point to control graph sizes.
+    
+    Adjust the size attribute to control how big to render images
+    """
+    fig, ax = plt.subplots(rows, cols, figsize=(size*cols, size*rows))
+    fig.tight_layout()
+    return ax
+
+# Load validation dataset
+dataset = nucleus.NucleusDataset()
+dataset.load_nucleus(DATASET_DIR, "stage1_test")
+
+dataset.prepare()
+
+print("Images: {}\nClasses: {}".format(len(dataset.image_ids), dataset.class_names))
+
+# Create model in inference mode
+with tf.device(DEVICE):
+    model = modellib.MaskRCNN(mode="inference",
+                              model_dir=LOGS_DIR,
+                              config=config)
+
+# Path to a specific weights file
+# weights_path = "../../kaggle_bowl.h5"
+weights_path = "kaggle_bowl.h5"
+
+# Load weights
+print("Loading weights ", weights_path)
+model.load_weights(weights_path, by_name=True)
+
+image_id = random.choice(dataset.image_ids)
+image, image_meta, gt_class_id, gt_bbox, gt_mask =\
+    modellib.load_image_gt(dataset, config, image_id, use_mini_mask=False)
+info = dataset.image_info[image_id]
+print("image ID: {}.{} ({}) {}".format(info["source"], info["id"], image_id, 
+                                       dataset.image_reference(image_id)))
+print("Original image shape: ", modellib.parse_image_meta(image_meta[np.newaxis,...])["original_image_shape"][0])
+
+# regular detection function
+print("object detection using detect()")
+img_name = str(image_id) + ".png" # ".jpg"
+import utils.utils as utils
+import utils.visualize as visualize
+coco_class_names = ['BG', 'cell']
+RESULT_DIR = os.path.join(ROOT_DIR, "mrcnn_result")
+regular_results = model.detect([image], verbose=1)
+reg_r = regular_results[0]
+# Visualize results
+visualize.save_image(image, img_name, reg_r['rois'], reg_r['masks'],
+  reg_r['class_ids'], reg_r['scores'], coco_class_names,
+  filter_classs_names=None, scores_thresh=0.1, 
+  save_dir=RESULT_DIR, mode=1)
+
+print("Done")
+'''
